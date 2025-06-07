e3301ffd7039a03127b4b8d7af43b29894a86fd7
Gustavo <gustavinho_ulp@hotmail.com>
2025-06-06 22:18:21 -0300

    Adição de blootstrap e poucas alterações no geral

--- Filmes/index.php
+++ Filmes/index.php
@@ -3,9 +3,32 @@ include "cabecalho.php";
 include "banner.php";
 
 ?>
+    <div class="container">
+        <h2 class="display-5">Melhores avaliações</h2>
+        <div class="row">
+            <div class="col-3">
+                <img src="imagem/Logan_Poster.jpg" width="300" height="500">
+                <a href="umfilme.php"><h3 class="filme-texto">Logan</h3></a>
+                <span class="filme-texto">⭐ 10/10</span>
+            </div>
+            <div class="col-3">
+                <img src="imagem/matrix.jfif" width="300" height="500">
+                <h3 class="filme-texto">Matrix</h3>
+                <span class="filme-texto">⭐ 10/10</span>
+            </div>
+            <div class="col-3">
+                <img src="imagem/em_ritmo_de_fuga.jpg" width="300" height="500">
+                <h3 class="filme-texto">Em Ritmo De Fuga</h3>
+                <span class="filme-texto">⭐ 10/10</span>
+            </div>
+            <div class="col-3">
+                <img src="imagem/capita_marvel.jfif" width="300" height="500">
+                <h3 class="filme-texto">Capita Marvel</h3>
+                <span class="filme-texto">⭐ 10/10</span>
+            </div>
+            
+        </div>
 
-    <h2 class="subtitulo">Melhores avaliações</h2>
-    
     
 <?php 
 include "rodape.php";
--- Filmes/rodape.php
+++ Filmes/rodape.php
@@ -3,5 +3,7 @@
         <img src="imagem/logo.png " height="150">
         <small><?php echo date("Y"); ?></small>
     </footer>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
