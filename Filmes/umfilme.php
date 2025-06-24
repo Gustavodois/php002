03e732ccb069384b07a22bf18a877c7a86910fb9
Gustavo <gustavinho_ulp@hotmail.com>
2025-06-23 21:09:36 -0300

    finalização do php e poucas alterações

--- Filmes/umfilme.php
+++ Filmes/umfilme.php
@@ -1,4 +1,15 @@
-<?php include "cabecalho.php"; ?>
+<?php include "cabecalho.php"; 
+
+  //1. Recuperar informação do id
+  //2. Conexão com o BD
+  //3. Montagem do sql
+  //4. Execução do sql
+  //5. Definição das variaveis do filme
+  //6. Laço com as informações do filme
+  //7. Mostrar as informações na tela
+  //8. Fechar a conexão com o BD
+
+?>
 <?php
         /* inicio da conexão com o BD*/
         include "conexao.php";
@@ -17,13 +28,23 @@
         //exit();
         while($linha = mysqli_fetch_assoc($resultado)){
           ?>
-          <div class="container text-center">
-            <h2><?=$linha['titulo'];?></h2>
-            <img src="<?=$linha['foto'];?> " style="width: 18rem;">
-            <p>⭐ <?=$linha['avaliacao'];?>/10</p>
-            <p><strong>Categoria: </strong> <?=$linha['categoria'];?> </p>
-            <p><strong>História:</strong> <?=$linha['historia'];?></p>
-        </div>
+        
+        <div class="container text-center"> 
+            <div class="row align-items-end"> 
+              <div class="col">
+              <h2><?=$linha['titulo'];?></h2>
+              <img src="<?=$linha['foto'];?> " style="width: 18rem;" class="img-fluid capa-filme">
+              </div>
+              <div class="col">
+              <p>⭐ <?=$linha['avaliacao'];?>/10</p>
+              <p><strong>Categoria: </strong> <?=$linha['categoria'];?> </p>
+              <p><strong>História:</strong> <?=$linha['historia'];?></p>
+              </div>
+            </div>
+           
+
+
+              
         <?php
         }
         mysqli_close($conexao);
